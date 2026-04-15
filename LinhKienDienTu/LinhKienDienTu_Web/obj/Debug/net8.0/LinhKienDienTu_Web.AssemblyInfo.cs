24259b464d9d1677b706f75793ecf7a7762af519
giang.nh <giang.nh@SGFPC01>
2026-04-15 20:55:49 +0700

    xóa rác hệ thống

--- LinhKienDienTu/LinhKienDienTu_Web/obj/Debug/net8.0/LinhKienDienTu_Web.AssemblyInfo.cs
+++ LinhKienDienTu/LinhKienDienTu_Web/obj/Debug/net8.0/LinhKienDienTu_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinhKienDienTu_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1a39560d6f38ca024cce5bf2837c5501e81927")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61053b0dc4477202c27b296d65d7b0040e107fd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinhKienDienTu_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinhKienDienTu_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
